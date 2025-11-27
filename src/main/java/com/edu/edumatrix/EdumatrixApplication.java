cd5190a12c62508dd8351c2aae09cbdda75cfa49
Thisara Thejana <thisarathejana20@gmail.com>
2025-11-27 09:51:29 +0530

    first commit

---
+++ src/main/java/com/edu/edumatrix/EdumatrixApplication.java
@@ -0,0 +1,13 @@
+package com.edu.edumatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdumatrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdumatrixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/edumatrix/EdumatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.edumatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdumatrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
